6b3e4c3b243d2f39bd1b0681c5b1802f720d673a
Suman-KC <kcsuman2058@gmail.com>
2025-11-05 04:32:14 +0545

    dashboard removed for now

--- unity-ed-frontend/unity-ed/app/dashboard/settings/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SettingsPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-semibold mb-4">Settings</h1>
-      <p>Adjust your preferences here.</p>
-    </div>
-  );
-}
